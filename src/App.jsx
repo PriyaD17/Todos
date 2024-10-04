9ab0d33ef213f5e3ca114fdcc7f276552a1fcede
Priyad17 <ganapathipriyadharsani@gmail.com>
2024-10-04 18:57:35 +0530

    added dormant basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <Enter/>
+      
+      
+    </div>
+  )
+}
+function Enter(){
+  const [task, setTask]= useState('');
+  const [desc, setDesc]= useState('');
+
+
+  return(
+    <div>
+      <header>The Todo App</header>
+      <p>Input Todo</p>
+     <br /><br /><br />
+     <input type="text" className='task' placeholder='Todo' value={task} onChange={(e)=> setTask(e.target.value)}/>
+     <br /><br />
+
+      <input type="text" className='desc' placeholder='Description' value= {desc} onChange={(e)=> setDesc(e.target.value)}/>
+      <br /><br />
+      <button onClick={() => console.log({ task, description })}>Add Todo</button>
+    </div>
   )
 }
 
