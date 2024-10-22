49a5f64eb7ade6cf4836ed651d2d37907a15adf7
Priyad17 <ganapathipriyadharsani@gmail.com>
2024-10-22 17:16:59 +0530

    updated correct logic

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,50 @@ function App() {
     </div>
   )
 }
+
 function Enter(){
   const [task, setTask]= useState('');
   const [desc, setDesc]= useState('');
+  const [todo, setTodo]= useState([]);
 
+  const addtodo=()=> {
+    if(task && desc){
+      const newtodo= { task, desc};
+      setTodo([...todo,newtodo]);
+      setTask('');
+      setDesc('');
+    }
+  }
 
   return(
     <div>
-      <header>The Todo App</header>
-      <p>Input Todo</p>
+      <header>The Todo App</header> <br />
+      <b>Input Todo</b>
      <br /><br /><br />
      <input type="text" className='task' placeholder='Todo' value={task} onChange={(e)=> setTask(e.target.value)}/>
      <br /><br />
 
       <input type="text" className='desc' placeholder='Description' value= {desc} onChange={(e)=> setDesc(e.target.value)}/>
       <br /><br />
-      <button onClick={() => console.log({ task, description })}>Add Todo</button>
+      <button onClick={addtodo}>Add Todo</button>
+
+
+      <div>
+
+        <br /><br /><br />
+
+        <h3>TODO List</h3>
+        <ul>
+          {todo.map((onetodo,index) => 
+         ( <li key={index}>
+            <strong> {onetodo.task}</strong>: {onetodo.desc}
+          </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+
+  );
 }
 
 export default App
